70e3c725f60d0d7f0224bff5d7af0b2b83a69d9d
Vishu Tyagi <39157981+strider187@users.noreply.github.com>
2020-08-23 19:33:59 +0530

    Update settings.py

--- simpleproject/simpleproject/settings.py
+++ simpleproject/simpleproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$ct&9v-f(x13(y$449v0!8=n51lt*v$)kzwk&sae!fb61s7twx'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
